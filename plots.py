edf1ec67be453203a9bb594bdbab0ccb6ae2ecea
Никита <dev.n.grishin@gmail.com>
2023-02-07 21:43:18 +0300

    Add plotly integration

--- plots.py
+++ plots.py
@@ -1,12 +1,15 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+import plotly.express as px
 
 from m_code_generator import McodeTdomain
 
 
 def get_m_code_t_domain_figure(m_code_t_domain: McodeTdomain):
-    plt.figure(figsize=(15, 5))
-    plt.plot(m_code_t_domain.t_arr, m_code_t_domain.m_code)
-    plt.xlabel("Время, с")
-    plt.ylabel("Амплитуда")
-    plt.title('M-последовательность во временной области')
-    plt.show()
+    x_title = 'Время, с'
+    y_title = 'Амплитуда'
+    df = {x_title: m_code_t_domain.t_arr,
+          y_title: m_code_t_domain.m_code}
+    fig = px.line(df, x=x_title, y=y_title,
+                  title="M-последовательность во временной области",
+                  height=500, width=1400)
+    fig.show()
