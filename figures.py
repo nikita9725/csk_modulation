d0cffbca0210e3a26deff342328565a0cd47a07e
Никита <dev.n.grishin@gmail.com>
2023-02-11 21:05:27 +0300

    Refactor

--- figures.py
+++ figures.py
@@ -5,8 +5,8 @@ from signal_processing.m_code_generator import McodeTdomain
 def get_m_code_t_domain_figure(m_code_t_domain: McodeTdomain):
     x_title = 'Время, с'
     y_title = 'Амплитуда'
-    df = {x_title: m_code_t_domain.t_arr,
-          y_title: m_code_t_domain.m_code}
+    df = {x_title: m_code_t_domain.t_arr_exp,
+          y_title: m_code_t_domain.m_code_exp}
     fig = px.line(df, x=x_title, y=y_title,
                   title="M-последовательность во временной области",
                   height=500, width=1400)
--- signal_processing/m_code_generator.py
+++ signal_processing/m_code_generator.py
@@ -2,6 +2,7 @@ import numpy as np
 from bitarray import bitarray
 from dataclasses import dataclass
 from math import ceil
+from functools import cache
 
 from const import McodeParams
 
@@ -9,33 +10,36 @@ from const import McodeParams
 @dataclass
 class McodeTdomain:
     m_code: np.array
+    m_code_exp: np.array
+    t_arr_exp: np.array
     t_arr: np.array
-    tau_simb: float
-    tau_simb_exp: float
+    tau_chip: float
+    tau_chip_exp: float
     t_period: float
 
     def __repr__(self):
         return (f'М-последовательность во временной области со следующими '
                 f'параметрами: \n'
-                f'Длительность символа: {self.tau_simb}\n'
+                f'Длительность символа: {self.tau_chip}\n'
                 f'Длительность символа (расширенная для графика) '
-                f'{self.tau_simb_exp}\n'
+                f'{self.tau_chip_exp}\n'
                 f'Период последовательности: {self.t_period}')
 
 
 class McodeGenerator:
     def __init__(self):
-        self.start_bits = McodeParams.start_bits
-        self.t_period = McodeParams.t_period
+        self.start_bits = McodeParams.START_BITS
+        self.t_period = McodeParams.T_PERIOD
 
         # Расширение кодовой последовательности
-        self.expans = McodeParams.expans
+        self.expans = McodeParams.EXPANS
 
         self.n = len(self.start_bits)
 
         # Максимальная длина последовательности
         self.m = 2 ** self.n - 1
 
+    @cache
     def generate_m_code(self) -> np.array:
         start_bits = bitarray(self.start_bits)
         m_code = np.array([], dtype='int')
@@ -50,18 +54,23 @@ class McodeGenerator:
 
         return m_code
 
+    @cache
     def get_m_code_in_t_domain(self) -> McodeTdomain:
         m_code = self.generate_m_code()
-        tau_simb = self.t_period / len(m_code)
-        tau_simb_exp = tau_simb / self.expans
-        n = ceil(self.t_period / tau_simb_exp)
-        t_arr = np.arange(start=0, step=tau_simb_exp, stop=n * tau_simb_exp)
-        m_code = np.repeat(m_code, self.expans)
+        tau_chip = self.t_period / len(m_code)
+        tau_chip_exp = tau_chip / self.expans
+        n = ceil(self.t_period / tau_chip_exp)
+        t_arr = np.arange(start=0, step=tau_chip, stop=n * tau_chip_exp)
+        t_arr_exp = np.arange(
+            start=0, step=tau_chip_exp, stop=n * tau_chip_exp)
+        m_code_exp = np.repeat(m_code, self.expans)
 
         return McodeTdomain(
             m_code=m_code,
+            m_code_exp=m_code_exp,
             t_arr=t_arr,
-            tau_simb=tau_simb,
-            tau_simb_exp=tau_simb_exp,
+            t_arr_exp=t_arr_exp,
+            tau_chip=tau_chip,
+            tau_chip_exp=tau_chip_exp,
             t_period=self.t_period
         )
