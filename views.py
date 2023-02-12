6f0fd12aa2245ffc2954ab790d234c9a9f606889
Никита <dev.n.grishin@gmail.com>
2023-02-12 17:49:24 +0300

    Add show_csk_code view

--- views.py
+++ views.py
@@ -16,3 +16,16 @@ def show_m_code():
     fig = get_m_code_t_domain_figure(m_code_t_domain)
 
     return dcc.Graph(figure=fig)
+
+
+@HtmlDivRegister()
+def show_csk_code():
+    m_code_gen = McodeGenerator()
+    m_code_t_domain = m_code_gen.get_m_code_t_domain()
+
+    csk_modulatior = CskModulator(m_code_t_domain)
+    message = (0, 0, 3, 3, 1, 1)
+    csk_t_domain = csk_modulatior.modulate_t_domain(message)
+    fig = get_csk_code_t_domain_figure(csk_t_domain)
+
+    return dcc.Graph(figure=fig)
