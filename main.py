20097211bc478dae9c3971307dfb98af30e99572
Никита <dev.n.grishin@gmail.com>
2023-02-24 17:52:02 +0300

    Flush cache option refactor

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import diskcache as dc
 import click
+import os
 
 from dash import Dash, html
 
+from const import CacheParams
 from utils import AppContainer, HtmlDivRegister
+
+
+def flush_cache_func():
+    cache_dir = CacheParams.CACHE_DIR
+    for file in os.listdir(cache_dir):
+        os.remove(os.path.join(cache_dir, file))
 
 
 def init_views():
@@ -22,13 +30,12 @@ def init_app(app: Dash):
 @click.option('--flush-cache', is_flag=True, default=False,
               help='Starting app with this flag flushes disk cache.')
 def start_app(flush_cache: bool):
+    if flush_cache is True:
+        flush_cache_func()
+
     app = Dash(__name__)
     init_app(app)
     app.layout = html.Div(HtmlDivRegister().divs)
-
-    if flush_cache is True:
-        cache: dc.Cache = AppContainer().cache
-        cache.clear()
 
     app.run_server(debug=True)
 
